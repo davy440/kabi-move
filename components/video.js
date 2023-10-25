a3289fa21720f8909b9cdfa61aff694c98477b64
Divjot <divjotdavy@gmail.com>
2023-10-24 22:19:35 -0400

    added autoplay to video

--- components/video.js
+++ components/video.js
@@ -1,7 +1,7 @@
 export const Video = ({ video }) => {
     return (
         <div className="kabi-movement-player absolute h-screen w-screen overflow-hidden">
-            <video className="h-full mx-auto">
+            <video className="h-full mx-auto" sutoPlay>
                 <source src={video} type="video/mp4" />
                 Your Browser does not support the video Tag.
             </video>
